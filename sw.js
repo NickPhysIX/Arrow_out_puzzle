d047b0174b90271cfeb46d7af55cb5b20ea29de6
Niels <n.braakman@gmail.com>
2026-07-01 14:46:13 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Arrow Out - offline app shell. Bump CACHE when you change files.
+const CACHE = 'arrow-out-best-v2';
+const ASSETS = ['./','index.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res => {
+      const url = new URL(req.url);
+      if (url.origin === location.origin) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+      }
+      return res;
+    }).catch(() => caches.match('index.html')))
+  );
+});
