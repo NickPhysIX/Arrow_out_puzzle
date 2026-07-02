053eee93ccde5f7de99481491f1dd9e589d25387
Niels <n.braakman@gmail.com>
2026-07-02 08:01:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Arrow Out - offline app shell. Bump CACHE when you change files.
-const CACHE = 'arrow-out-long-snakes-v16';
+const CACHE = 'arrow-out-organic-v19';
 const ASSETS = ['./','index.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png'];
 
 self.addEventListener('install', e => {
